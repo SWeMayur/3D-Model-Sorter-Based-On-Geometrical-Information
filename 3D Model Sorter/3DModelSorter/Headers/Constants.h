d724543085e453a913f7da10f545b9e424da9ba7
Mayur Kapadnis <149293586+SWeMayur@users.noreply.github.com>
2024-01-15 15:18:01 +0530

    Update Constants.h

--- 3D Model Sorter/3DModelSorter/Headers/Constants.h
+++ 3D Model Sorter/3DModelSorter/Headers/Constants.h
@@ -1,1 +1,18 @@
 
+#pragma once
+#include <qstring.h>
+class Constants
+{
+public:
+	QString mAddFileButtonStyle;
+	QString mTableStyle;
+	QString mFileSelectButtonStyle;
+	QString mShowModelButtonStyle;
+	QString mParameterListButtonStyle;
+	QString mSortButtonStyle;
+
+public:
+	Constants();
+	~Constants();
+};
+
